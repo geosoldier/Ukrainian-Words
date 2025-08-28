9a924c1150162d0c0a6074dd4918ef9092a18f9f
geosoldier <89037538+geosoldier@users.noreply.github.com>
2025-08-29 00:08:21 +0100

    Initial Commit

---
+++ Ukrainian Words/Ukrainian_WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ukrainian_WordsApp.swift
+//  Ukrainian Words
+//
+//  Created by Eric Adams on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Ukrainian_WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ukrainian WordsTests/Ukrainian_WordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ukrainian_WordsTests.swift
+//  Ukrainian WordsTests
+//
+//  Created by Eric Adams on 8/29/25.
+//
+
+import Testing
+@testable import Ukrainian_Words
+
+struct Ukrainian_WordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
